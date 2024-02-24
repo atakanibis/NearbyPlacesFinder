01eebdfbb8b9dd786e7df2f8f9a07b9bbafeb74e
Atakanibis <atakanibis1@gmail.com>
2024-02-25 02:31:35 +0300

    project architecture was created.

---
+++ src/main/java/com/nearbyplacesfinder/nearbyplacesfinder.java
@@ -0,0 +1,12 @@
+package com.nearbyplacesfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyPlacesFinderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NearbyPlacesFinderApplication.class, args);
+    }
+}
