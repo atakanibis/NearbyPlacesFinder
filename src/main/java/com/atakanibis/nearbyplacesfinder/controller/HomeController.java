367886adb0cb718bac405a3aed1f7b40175af6ca
Atakanibis <atakanibis1@gmail.com>
2024-02-26 20:28:11 +0300

    updated html,css and js file for google maps api

--- src/main/java/com/atakanibis/nearbyplacesfinder/controller/HomeController.java
+++ src/main/java/com/atakanibis/nearbyplacesfinder/controller/HomeController.java
@@ -1,13 +1,17 @@
 package com.atakanibis.nearbyplacesfinder.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-
+    @Value("${google.places.api.key}")
+    private String googleApiKey;
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("googleApiKey", googleApiKey);
         return "index";
     }
 }
